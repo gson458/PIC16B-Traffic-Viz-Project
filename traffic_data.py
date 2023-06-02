6484a9c1d36f263630d368259d476bbb1f0d768f
gabo458 <81783303+gabo458@users.noreply.github.com>
2023-06-01 22:32:53 -0700

    Added counties and heatmap to traffic_data

--- traffic_data.py
+++ traffic_data.py
@@ -2,7 +2,8 @@ import requests
 import json
 import sqlite3
 import time
-import folium
+import ast
+# import folium
 import pandas as pd
 from plotly import express as px
 from datetime import datetime, timedelta
@@ -12,11 +13,75 @@ from datetime import datetime, timedelta
 #create our own database
 conn = sqlite3.connect('traffic_data.db')
 
+
+def extract_coordinates(geo_shape):
+    """Helper function to extract latitude and longitude from coordinates"""
+    geo_dict = ast.literal_eval(geo_shape)
+    coordinates = []
+
+    if geo_dict['type'] == 'Polygon':
+        coordinates = geo_dict['coordinates'][0]
+    elif geo_dict['type'] == 'MultiPolygon':
+        for polygon in geo_dict['coordinates']:
+            coordinates.extend(polygon[0])
+    elif geo_dict['type'] == 'Point':
+        coordinates = [geo_dict['coordinates']]
+
+    latitudes = [coord[1] for coord in coordinates]
+    longitudes = [coord[0] for coord in coordinates]
+    return latitudes, longitudes
+
+def initialize_db(conn):
+    """Creates a counties table in the database if one does not already exist"""
+    cmd =\
+    """
+    SELECT name FROM sqlite_master WHERE name='counties'
+    """
+    cursor = conn.cursor()
+    cursor.execute(cmd)
+    if len(cursor.fetchall()) < 1:
+        url = "https://public.opendatasoft.com/explore/dataset/us-county-boundaries/download/?format=csv&refine.statefp=06&location=2,40.61349,40.02538&timezone=America/Los_Angeles&lang=en&use_labels_for_header=true&csv_separator=%3B"
+
+        df = pd.read_csv(url, sep=";")
+        new_df = df[["NAME", "Geo Shape"]].copy()
+
+        # Create a list to store county bounds dataframes
+        county_bounds_list = []
+
+        # Iterate over each county
+        for _, row in new_df.iterrows():
+            county_name = row['NAME']
+            latitudes, longitudes = extract_coordinates(row['Geo Shape'])
+
+            # Calculate the minimum and maximum latitude and longitude for the county
+            min_latitude = min(latitudes)
+            max_latitude = max(latitudes)
+            min_longitude = min(longitudes)
+            max_longitude = max(longitudes)
+
+            # Create a dataframe for the county bounds
+            county_bounds_df = pd.DataFrame({'County': county_name,
+                                            'Min_Latitude': min_latitude,
+                                            'Max_Latitude': max_latitude,
+                                            'Min_Longitude': min_longitude,
+                                            'Max_Longitude': max_longitude}, index=[0])
+
+            # Append the county bounds dataframe to the list
+            county_bounds_list.append(county_bounds_df)
+
+        # Concatenate all county bounds dataframes into a single dataframe
+        county_bounds = pd.concat(county_bounds_list, ignore_index=True)
+
+        # Sort the DataFrame by county name
+        county_bounds = county_bounds.sort_values('County')
+
+        county_bounds.to_sql("counties", conn, if_exists="replace", index=False)
+
 def update(conn):
     """Updates incidents table by removing incidents with end times five hours before current time"""
 
     try:
-        cutoffTime = datetime.now() + timedelta(hours=24)
+        cutoffTime = datetime.now() - timedelta(hours=5)
         cursor = conn.cursor()
         cmd=\
         f"""
@@ -72,9 +137,10 @@ def get_traffic_data(bbox, key):
         data = data[['id', 'type', 'severity', 'shortDesc', 'lat', 'lng', 'startTime', 'endTime']]
     return data
 
-def store_traffic_data(conn, key, lat_start, lat_end, lng_start, lng_end):
+def store_traffic_data(conn, key, bbox):
     """Iterates over a given area and updates traffic incident database by MapQuest API calls"""
 
+    lat_start, lat_end, lng_start, lng_end = bbox
     bbox_step = 1  # Bbox step size for iterating over the U.S. 
     bbox_range = {
         # Starting latitude for bbox
@@ -115,6 +181,48 @@ def store_traffic_data(conn, key, lat_start, lat_end, lng_start, lng_end):
             bbox["lng_end"] = bbox_range["lng_start"] + bbox_step
         time.sleep(1)  # Sleep for 1 second to avoid hitting API rate limits
 
+def get_counties(conn):
+    """Returns a list of all counties in California"""
+
+    cmd =\
+    """
+    SELECT county FROM counties
+    """
+    cursor = conn.cursor()
+    cursor.execute(cmd)
+    return [i[0] for i in cursor.fetchall()]
+
+def get_county_incidents(conn, county):
+    """Returns a dataframe of all traffic incidents in given county in California"""
+
+    cmd =\
+    f"""
+    SELECT Min_Latitude, Max_Latitude, Min_Longitude, Max_Longitude FROM counties
+    WHERE County LIKE '{county.upper()}'
+    """
+    # print(cmd)
+    cursor = conn.cursor()
+    cursor.execute(cmd)
+    # min_lat, max_lat, min_lng, max_lng = cursor.fetchone()
+    bbox = cursor.fetchone()
+
+    return get_incidents_in_area(conn, bbox)
+
+def update_county_incidents(conn, key, county):
+    """Updates the database with current incidents in a given CA county"""
+
+    cmd =\
+    f"""
+    SELECT Min_Latitude, Max_Latitude, Min_Longitude, Max_Longitude FROM counties
+    WHERE County LIKE '{county.upper()}'
+    """
+    # print(cmd)
+    cursor = conn.cursor()
+    cursor.execute(cmd)
+    # min_lat, max_lat, min_lng, max_lng = cursor.fetchone()
+    bbox = cursor.fetchone()
+    store_traffic_data(conn, key, bbox)
+
 def get_incidents_in_area(conn, bbox):
     """Retrieves incidents within the given bounding box from database"""
     
@@ -130,8 +238,8 @@ def get_incidents_in_area(conn, bbox):
     df = pd.read_sql_query(cmd, conn)
     return df
 
-def display_map_with_incidents(incidents, **kwargs):
-    """Creates a plotly map of traffic incidents"""
+def incident_scattermap(incidents, **kwargs):
+    """Creates a plotly scattermap of traffic incidents"""
     
     # try:
     #     center_lat, center_lng = incidents['lat'][0], incidents['lng'][0]
@@ -143,6 +251,19 @@ def display_map_with_incidents(incidents, **kwargs):
                             lat="lat",
                             lon="lng",
                             color="severity",
+                            hover_name="id",
+                            hover_data=['shortDesc', 'type'],
+                            mapbox_style="open-street-map",
+                            **kwargs)
+    fig.update_layout(margin={"r":0, "l":0,"b":0,"t":0})
+    return fig
+
+def incident_heatmap(incidents, **kwargs):
+    """Creates a plotly heatmap of traffic incidents"""
+
+    fig = px.density_mapbox(incidents,
+                            lat="lat",
+                            lon="lng",
                             hover_name="id",
                             hover_data=['shortDesc', 'type'],
                             mapbox_style="open-street-map",
